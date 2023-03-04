b2db71bd9baabede7b2047c120351ac962b16e46
PreetomCho <preetom56@gmail.com>
2023-03-03 19:41:13 -0500

    implemented map method

--- javascript-methods.js
+++ javascript-methods.js
@@ -10,7 +10,12 @@ In this Assignment, we use the prototype constructor to add new methods to the A
 
 // MAP //
 Array.prototype.myMap = function(callbackFn) {
-  // Place your code here.
+  let x = [];
+  for(let i=0; i<array.length; i++) {
+    x.push(callbackFn(array[i],i,array));
+  }
+
+  return x;
 };
 
 // FILTER //
